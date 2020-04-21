3c422073a83cf01535891bf8e1d7aa9adbf03533
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-04-21 21:20:47 +0300

    Рефакторинг js для кнопки открытия мобильного меню

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -3,11 +3,11 @@
 var burger = document.querySelector('.main-nav__toggle');
 var dropDown = document.querySelector('.drop-down');
 var header = document.querySelector('.header');
-var logo = document.querySelector('.header__logo img');
+var logo = document.querySelector('.header__logo');
 
 burger.addEventListener('click', function () {
-  dropDown.classList.toggle('open');
-  burger.classList.toggle('open');
-  header.classList.toggle('open');
-  logo.src = dropDown.classList.contains('open') ? 'img/logo/logo-mobile-blue@1x.png' : 'img/logo/logo-mobile-white@1x.png';
+  dropDown.classList.toggle('drop-down--open');
+  burger.classList.toggle('main-nav__toggle--open');
+  header.classList.toggle('header--open');
+  logo.classList.toggle('logo--open');
 });
