7c06449db4460e13197c92571c31b38405f273a9
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-05-18 15:43:43 +0300

    Добавляет списку выбора стран на странице формы состояние "Открыто" при выключенном js

--- source/js/modules/addCountry.js
+++ source/js/modules/addCountry.js
@@ -5,6 +5,9 @@
   const addCountry = document.querySelector('.select-menu');
 
   if (addCountry) {
+
+    addCountry.classList.remove('visit__item--nojs');
+
     addCountry.addEventListener('click', function () {
       addCountry.classList.toggle('visit__item--open');
     });
