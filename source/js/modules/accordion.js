898fd93a74af21ac204f3fc682564fc205167f4f
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-05-26 20:48:23 +0300

    Добавляет доступность содержимого фильтра попутчиков при выключенном js на странице каталога

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -2,17 +2,18 @@
 
 (function () {
 
-  let form = document.querySelector('.traveler-filter__form');
+  const form = document.querySelector('.traveler-filter__form');
 
   if (form) {
-    let btn = form.querySelectorAll('.traveler-filter__legend');
-    let panel = form.querySelectorAll('.traveler-filter__list');
+
+    const btn = form.querySelectorAll('.traveler-filter__legend');
+
+    btn.classList.remove('traveler-filter__legend--nojs');
 
     for (let i = 0; i < btn.length; i++) {
       btn[i].addEventListener('click', function (e) {
         e.preventDefault();
         btn[i].classList.toggle('traveler-filter__legend--open');
-        panel[i].classList.toggle('traveler-filter__list--open');
       });
     }
   }
