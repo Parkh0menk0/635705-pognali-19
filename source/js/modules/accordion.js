47d47dffd5c121599778f0efcf05b9bb89131a08
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-05-26 21:15:29 +0300

    Рефакторинг для фильтра

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -8,9 +8,8 @@
 
     const btn = form.querySelectorAll('.traveler-filter__legend');
 
-    btn.classList.remove('traveler-filter__legend--nojs');
-
     for (let i = 0; i < btn.length; i++) {
+      btn[i].classList.remove('traveler-filter__legend--nojs');
       btn[i].addEventListener('click', function (e) {
         e.preventDefault();
         btn[i].classList.toggle('traveler-filter__legend--open');
